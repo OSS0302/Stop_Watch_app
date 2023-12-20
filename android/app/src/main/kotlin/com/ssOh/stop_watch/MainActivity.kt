e0bb82183a97d1a7ff61c6142ea0e8bd6329b460
Ohsangsoo <tkdtn3264@gmail.com>
2023-12-20 20:24:40 +0900

    first-commit

---
+++ android/app/src/main/kotlin/com/ssOh/stop_watch/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ssOh.stop_watch
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/stop_watch_screen.dart
@@ -0,0 +1,13 @@
+class StopWatchS extends StatefulWidget {
+  const StopWatchS({super.key});
+
+  @override
+  State<StopWatchS> createState() => _StopWatchSState();
+}
+
+class _StopWatchSState extends State<StopWatchS> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
