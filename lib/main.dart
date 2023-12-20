282aa349310a2bc56e6a247af4719cecb9956e79
Ohsangsoo <tkdtn3264@gmail.com>
2023-12-20 20:32:25 +0900

    067. 시작 일시정지 버튼 ui 완성

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:stop_watch/stop_watch_screen.dart';
 
 void main() {
   runApp(const MyApp());
--- lib/stop_watch_screen.dart
+++ lib/stop_watch_screen.dart
@@ -10,6 +10,32 @@ class StopWatchScreen extends StatefulWidget {
 class _StopWatchScreenState extends State<StopWatchScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('스탑워치'),
+      ),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              FloatingActionButton(
+                backgroundColor: Colors.orange,
+                onPressed: () {},
+                child: const Icon(Icons.refresh),
+              ),FloatingActionButton(
+                backgroundColor: Colors.blue,
+                onPressed: () {},
+                child: const Icon(Icons.play_arrow),
+              ),FloatingActionButton(
+                backgroundColor: Colors.green,
+                onPressed: () {},
+                child: const Icon(Icons.add),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
